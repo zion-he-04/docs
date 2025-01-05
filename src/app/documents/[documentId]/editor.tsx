2e22473cac37e7e7be28825312ca78d40e10a572
Zheyu <z5487658@ad.unsw.edu.au>
2025-01-05 11:16:50 +1100

    Feat: extensions and css for images, tables, bullet points, etc..

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { useEditor, EditorContent } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image';
 
 export const Editor = () => {
   const editor = useEditor({
@@ -11,8 +19,35 @@ export const Editor = () => {
             class: "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
         },
     },
-    extensions: [StarterKit],
-    content: '<p>hello bello</p>',
+    extensions: [
+        StarterKit,
+        TaskItem.configure({
+            nested: true,
+        }),
+        TaskList,
+        Table,
+        TableCell,
+        TableHeader,
+        TableRow,
+        Image,
+        ImageResize,
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
   });
 
   return (
