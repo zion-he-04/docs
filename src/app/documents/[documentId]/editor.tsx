c4a9edf85806f0deec74308362a402c65a8197b2
Zheyu <z5487658@ad.unsw.edu.au>
2025-02-16 13:17:13 +1100

    sfs

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -40,6 +40,7 @@ export const Editor = ({ initialContent}: EditorProps) => {
 	const { setEditor }  = useEditorStore();
 	
 	const editor = useEditor({
+		autofocus: true,
 		immediatelyRender: false,
 		onCreate({ editor }) {
 			setEditor(editor);
